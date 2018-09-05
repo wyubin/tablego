bb0011ebd27002458a62838debaa726cf31e9858
wyubin <yubin0611@gmail.com>
2018-09-05 16:58:02 +0800

    add maxLine to Scanner

--- table.go
+++ table.go
@@ -10,12 +10,14 @@ import (
 type Scanner struct {
 	bufio    bufio.Scanner
 	spliter  string
+	maxLine  int
 	colnames []string
 }
 
 // default value of Scanner
 const (
 	spliter = "\t"
+	maxLine = 0
 )
 
 // Input - input table file object(io.Reader)
@@ -23,22 +25,27 @@ func Input(r io.Reader) *Scanner {
 	return &Scanner{
 		bufio:   *bufio.NewScanner(r),
 		spliter: spliter,
+		maxLine: maxLine,
 	}
 }
 
 // Iter - return []string by channel
 func (s *Scanner) Iter() <-chan []string {
 	chnl := make(chan []string)
+	countLine := 0
 	go func() {
 		for s.bufio.Scan() {
 			tmpLine := s.bufio.Text()
-			if len(tmpLine) == 0 {
+			if (s.maxLine != 0) && (countLine > s.maxLine) {
+				break
+			} else if len(tmpLine) == 0 {
 				continue
 			} else if tmpLine[:1] == "#" {
 				s.colnames = strings.Split(tmpLine[1:], s.spliter)
 			} else {
 				chnl <- strings.Split(s.bufio.Text(), s.spliter)
 			}
+			countLine++
 		}
 		close(chnl)
 	}()
